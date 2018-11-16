9a04e5c8de21b8e2c36d284b2732d7f7ddbaf95b
Kobus Swart <kobus@hearxgroup.com>
2018-11-16 09:13:41 +0200

    Check change

--- showcaseviewlib/src/main/java/smartdevelop/ir/eram/showcaseviewlib/GuideView.java
+++ showcaseviewlib/src/main/java/smartdevelop/ir/eram/showcaseviewlib/GuideView.java
@@ -315,7 +315,7 @@ public class GuideView extends LinearLayout {
 
     void setTextViewNextPosition(int x, int y) {
 
-        skipRect = new RectF(x * 0.8f,y * 0.85f ,x * 0.97f , y * 0.97f );
+        skipRect = new RectF(x * 0.8f,y * 0.85f ,x * 0.99f , y * 0.99f );
 
         if (showArrows) {
 
